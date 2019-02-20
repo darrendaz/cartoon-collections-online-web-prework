98fe3a574b094727c516d45c367e8910fe47f10c
Darren L <darren.lou@gmail.com>
2019-02-20 21:15:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(arrayCalls)
-  return arrayCalls.any? {|call| call.length >= 4 }
+  arrayCalls.any? {|call| call.length > 4 }
   } 
 end
 
